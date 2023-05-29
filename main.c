7ad765358d17339718707d85a6c98fcff8537dea
Alessandro Carminati <alessandro.carminati@gmail.com>
2023-05-29 12:56:07 +0200

    pid in messages
    Signed-off-by: Alessandro Carminati <alessandro.carminati@gmail.com>

--- main.c
+++ main.c
@@ -8,29 +8,31 @@ int main(int argc, char *argv[]) {
 	char *endptr;
 	int value, cpu;
 	cpu_set_t cpuset;
+	pid_t pid=getpid();
 
-	printf("CPU alive tester\n");
+	printf("[%d] CPU alive tester\n", pid);
 	if (argc != 2) {
-		printf("Usage: %s <integer>\n", argv[0]);
+		printf("[%d] Usage: %s <integer>\n", pid, argv[0]);
 		return 1;
 		}
 	value = strtol(argv[1], &endptr, 10);
 	if (*endptr != '\0') {
-		printf("Invalid input: '%s' is not a valid integer.\n", argv[1]);
+		printf("[%d] Invalid input: '%s' is not a valid integer.\n", pid, argv[1]);
 		return 1;
 		}
 
 	CPU_ZERO(&cpuset);
 	if (sched_getaffinity(0, sizeof(cpu_set_t), &cpuset) == -1) {
-		perror("sched_getaffinity");
+		printf("[%d] sched_getaffinity", pid);
 		return 1;
 		}
 
-	printf("Process is allowed to run on the following CPUs:\n");
+	printf("[%d] Process is allowed to run on the following CPUs:\n", pid);
 	for (cpu = 0; cpu < CPU_SETSIZE; cpu++) if (CPU_ISSET(cpu, &cpuset)) printf("CPU %d\n", cpu);
 
-	printf("Sleeps for %d second(s)\n", value);
+	printf("[%d] Sleeps for %d second(s)\n", pid, value);
+	fflush(stdout);
     	sleep(value);
-	printf("Current CPU: %d\n", sched_getcpu());
+	printf("[%d] Current CPU: %d\n", pid, sched_getcpu());
 	return 0;
 }
